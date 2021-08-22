907a4cb9a0bd3268cfa40b56e1a181fc9ec86823
MoskalevaMarina <mari10102008@yandex.ru>
2021-08-22 22:22:30 +0400

    Добавьте файлы проекта.

---
+++ ItogovProject14/Contact.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="Contact.cs" company="My Company Marina">
+// Copyright (c) 2021 All Rights Reserved
+// </copyright>
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItogovProject14
+{
+    /// <summary>
+    /// Класс Contact
+    /// </summary>
+    public class Contact
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Contact"/> class.
+        /// </summary>
+        /// <param name="name">Ввод имени</param>
+        /// <param name="lastName">Ввод фамилии</param>
+        /// <param name="phoneNumber">Ввод номера телефона</param>
+        /// <param name="email">Ввод email</param>
+        public Contact(string name, string lastName, long phoneNumber, string email)
+        {
+            this.Name = name;
+            this.LastName = lastName;
+            this.PhoneNumber = phoneNumber;
+            this.Email = email;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether поле для хранения имени
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether поле для хранения фамилии
+        /// </summary>
+        public string LastName { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether поле для хранения номера телефона
+        /// </summary>
+        public long PhoneNumber { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether поле для хранения email
+        /// </summary>
+        public string Email { get; }
+    }
+}
---
+++ ItogovProject14/Program.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="Program.cs" company="My Company Marina">
+// Copyright (c) 2021 All Rights Reserved
+// </copyright>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ItogovProject14
+{
+    /// <summary>
+    /// Класс Program
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// метод Main
+        /// </summary>
+        public static void Main()
+        {
+            var phoneBook = new List<Contact>();
+
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            var phoneBooksort = phoneBook.OrderBy(s => s.Name).ThenBy(s => s.LastName);
+
+            while (true)
+            {
+                var input = Console.ReadKey().KeyChar;
+
+                var parsed = int.TryParse(input.ToString(), out int pageNumber);
+
+                if (!parsed || pageNumber < 1 || pageNumber > 3)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Страницы не существует");
+                }
+                else
+                {
+                    var pageContent = phoneBooksort.Skip((pageNumber - 1) * 2).Take(2);
+                    Console.WriteLine();
+
+                    foreach (var entry in pageContent)
+                    {
+                        Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
